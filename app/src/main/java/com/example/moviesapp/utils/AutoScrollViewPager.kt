286c3e65e0185285c64618d0e2564e4265361cc8
crazycotanak <Cotanak2828>
2021-08-21 02:13:27 +0300

    AutoScroll viewpager

---
+++ app/src/main/java/com/example/moviesapp/utils/AutoScrollViewPager.kt
@@ -0,0 +1,71 @@
+package com.example.moviesapp.utils
+
+import android.content.Context
+import android.os.Handler
+import android.os.Looper
+import android.util.AttributeSet
+import androidx.viewpager.widget.ViewPager
+
+class AutoScrollViewPager : ViewPager {
+
+    constructor(context: Context) : super(context) {
+
+    }
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+
+    }
+
+
+    private var mHandler: Handler = Handler(Looper.getMainLooper())
+
+    private var scrollPosition = 0
+    private var interval: Long = 0
+
+    private val runnable = object : Runnable {
+
+        override fun run() {
+
+            /**
+             * Calculate "scroll position" with
+             * adapter pages count and current
+             * value of scrollPosition.
+             */
+            val count = adapter?.count ?: 0
+            if (count > 0)
+                setCurrentItem(scrollPosition++ % count, true)
+
+            mHandler.postDelayed(this, interval)
+        }
+    }
+
+
+    fun startAutoScroll(interval: Long = 5000) {
+        this.interval = interval
+
+        addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageSelected(position: Int) {
+                // Updating "scroll position" when user scrolls manually
+                scrollPosition = position + 1
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+                // Not necessary
+            }
+
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+                // Not necessary
+            }
+        })
+
+        mHandler.post(runnable)
+    }
+
+    fun cancelAutoScroll() {
+        mHandler.removeCallbacks(runnable)
+    }
+}
