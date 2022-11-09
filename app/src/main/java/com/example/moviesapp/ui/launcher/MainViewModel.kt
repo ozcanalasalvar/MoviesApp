3a6bd2d5e6b92879ac935052991b5de9d94c8ad1
OzcanAlasalvar <ozcanalasalvarr@gmail.com>
2022-11-10 00:00:22 +0300

    removed unnecessary files

--- app/src/main/java/com/example/moviesapp/ui/launcher/MainViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.moviesapp.ui.launcher
-
-import com.example.moviesapp.data.repository.Repository
-import com.example.moviesapp.ui.base.BaseViewModel
-import javax.inject.Inject
-
-class MainViewModel @Inject constructor(private val repository: Repository):BaseViewModel() {
-}
