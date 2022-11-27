a415f3fe4124440f5108ae0ce422caa162727e8a
OzcanAlasalvar <ozcanalasalvarr@gmail.com>
2022-11-28 02:58:04 +0300

    groovy to kts

--- app/src/main/java/com/example/moviesapp/movies/MoviesScreen.kt
+++ app/src/main/java/com/example/moviesapp/movies/MoviesScreen.kt
@@ -4,12 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.moviesapp.adapters.MoviePagerAdapter
 import com.example.moviesapp.adapters.MovieSliderAdapter
